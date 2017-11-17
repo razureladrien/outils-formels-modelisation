b7a428637548fce2d3d8db8c38e20e1dcf0568da
razureladrien <adrien.razurel@etu.unige.ch>
2017-11-17 12:33:04 +0100

    tp4

--- tp-04/Philosophers/Sources/Philosophers/main.swift
+++ tp-04/Philosophers/Sources/Philosophers/main.swift
@@ -2,7 +2,7 @@ import PetriKit
 import PhilosophersLib
 
 do {
-    enum C: CustomStringConvertible {
+    enum C: CustomStringConvertible { //les types que je peux avoir
         case b, v, o
 
         var description: String {
@@ -14,9 +14,9 @@ do {
         }
     }
 
-    func g(binding: PredicateTransition<C>.Binding) -> C {
+    func g(binding: PredicateTransition<C>.Binding) -> C { //comme sur la diapo 29/44 du cours
         switch binding["x"]! {
-        case .b: return .v
+        case .b: return .v //g(b) = v
         case .v: return .b
         case .o: return .o
         }
@@ -24,18 +24,18 @@ do {
 
     let t1 = PredicateTransition<C>(
         preconditions: [
-            PredicateArc(place: "p1", label: [.variable("x")]),
+            PredicateArc(place: "p1", label: [.variable("x")]),//voir diapo 29/44 aussi
         ],
         postconditions: [
-            PredicateArc(place: "p2", label: [.function(g)]),
+            PredicateArc(place: "p2", label: [.function(g)]), //passe la fonction g au dessus sur le label x
         ])
 
     let m0: PredicateNet<C>.MarkingType = ["p1": [.b, .b, .v, .v, .b, .o], "p2": []]
-    guard let m1 = t1.fire(from: m0, with: ["x": .b]) else {
+    guard let m1 = t1.fire(from: m0, with: ["x": .b]) else { //le fire prend en parametre le binding cette fois : x vaut b
         fatalError("Failed to fire.")
     }
     print(m1)
-    guard let m2 = t1.fire(from: m1, with: ["x": .v]) else {
+    guard let m2 = t1.fire(from: m1, with: ["x": .v]) else { //x vaut v
         fatalError("Failed to fire.")
     }
     print(m2)
